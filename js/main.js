2c82cf60ad0570a8bde9c15b0a88527aa202efff
eangeles <niwre2007@gmail.com>
2013-12-04 16:53:11 -0500

    commiting fix for the head error

--- js/main.js
+++ js/main.js
@@ -13,22 +13,21 @@ wingaming.config(function ($routeProvider){
     $routeProvider
         .when("/",{
             title: 'Home',
-<<<<<<< HEAD
+
 /*             controller:"Login", */
-=======
->>>>>>> 5c1dcc5f9e6993b03e6de2a817aee4f2a3f92332
+
+
             templateUrl:"views/home.html"
         })
         .when("/under_construction", {
             title: 'Work in progress',
             templateUrl:"views/under_construction.html"
-<<<<<<< HEAD
+
         })
         .when("/gts", {
             title: 'Gamer Tracking System',
             templateUrl:"views/gts.html"
-=======
->>>>>>> 5c1dcc5f9e6993b03e6de2a817aee4f2a3f92332
+
         })
         .when("/crud_testing", {
             title: 'Crud Testing',
