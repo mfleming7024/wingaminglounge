3b319a968931b590e947e0253b52727b17df6e55
Michael Fleming <Michael@The-Original-Michaels-MacBook-Pro.local>
2013-12-05 22:06:25 -0500

    [delivers #62029086]
    Cleaned up console logs and also revamped the login to determine
    whether or not they exist and to add them only if they dont. Minor
    random code cleanup as well

--- js/controllers/LoginController.js
+++ js/controllers/LoginController.js
@@ -3,34 +3,49 @@ wingaming.controller('Login', ['$scope', '$routeParams', '$location', 'angularFi
 	var theUser;
 
     var urlUsers = new Firebase("https://wingaminglounge.firebaseio.com/wingaminglounge/users");
-	//collects the info from the database for use.
+
 	$scope.users = angularFireCollection(urlUsers);
 	
     $scope.$on("angularFireAuth:login", function(evt, user) {
         if (user.provider == "facebook") {            
             theUser = user;
         } else {
-	        console.log("Login other then facebook");
+	        console.log("Login other then the facebook service");
         }
     });
 
     $scope.login = function() {
 		angularFireAuth.login("facebook", {
 			scope: "email"			
-		}).then(function(){
-			console.log(theUser);
-		
-			var picurl = "http://graph.facebook.com/" + theUser.username + "/picture?type=small";
-	        var picurlLarge = "http://graph.facebook.com/" + theUser.username + "/picture?type=large";
+		}).then(function(){	
+			var userExists;		
+            for (var i = 0, max = $scope.users.length; i<max; i++) {
+                if ($scope.users[i].email != theUser.email) {
+                	userExists = false;
+                } else {
+                	userExists = true;
+                    break;
+                }
+            }
+            
+            if (!userExists) {
+	            console.log("User does not exist, adding " + theUser.email + " to the database.");
+	            
+	            var picurl = "http://graph.facebook.com/" + theUser.username + "/picture?type=small";
+		        var picurlLarge = "http://graph.facebook.com/" + theUser.username + "/picture?type=large";
+				
+				$scope.users.add({"displayName": theUser.name, "email": theUser.email, "profilePic": picurl, "profilePicLarge": picurlLarge});
+				$location.path('/gts');
+            } else {
+	            console.log("User does exist, not adding " + theUser.email + " to the database");
+            }
 			
-			$scope.users.add({"displayName": theUser.name, "email": theUser.email, "profilePic": picurl, "profilePicLarge": picurlLarge});
-			$location.path('/gts');
 		});		
     };
     
     $scope.logout = function() {
         angularFireAuth.logout();
-        $location.path('/')
+        $location.path('/');
     };
 
 }])
