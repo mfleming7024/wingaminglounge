7ed6529048cefdccf209705506e72fb666fd7640
Michael Fleming <Michael@The-Original-Michaels-MacBook-Pro.local>
2013-12-05 21:46:20 -0500

    [delivers #61920544]
    Reworked login controller to only run the add user once

--- js/controllers/LoginController.js
+++ js/controllers/LoginController.js
@@ -7,13 +7,8 @@ wingaming.controller('Login', ['$scope', '$routeParams', '$location', 'angularFi
 	$scope.users = angularFireCollection(urlUsers);
 	
     $scope.$on("angularFireAuth:login", function(evt, user) {
-    	console.log(user);
-    	console.log(user.email);
-        if (user.provider == "facebook") {
-            var picurl = "http://graph.facebook.com/" + user.username + "/picture?type=small";
-            var picurlLarge = "http://graph.facebook.com/" + user.username + "/picture?type=large";
-            $scope.users.add({"displayName": user.name, "email": user.email, "profilePic": picurl, "profilePicLarge": picurlLarge});
-            $location.path('/gts');
+        if (user.provider == "facebook") {            
+            theUser = user;
         } else {
 	        console.log("Login other then facebook");
         }
@@ -22,7 +17,15 @@ wingaming.controller('Login', ['$scope', '$routeParams', '$location', 'angularFi
     $scope.login = function() {
 		angularFireAuth.login("facebook", {
 			scope: "email"			
-		});
+		}).then(function(){
+			console.log(theUser);
+		
+			var picurl = "http://graph.facebook.com/" + theUser.username + "/picture?type=small";
+	        var picurlLarge = "http://graph.facebook.com/" + theUser.username + "/picture?type=large";
+			
+			$scope.users.add({"displayName": theUser.name, "email": theUser.email, "profilePic": picurl, "profilePicLarge": picurlLarge});
+			$location.path('/gts');
+		});		
     };
     
     $scope.logout = function() {
