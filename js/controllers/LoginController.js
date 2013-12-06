37aa8b49fbb8efab4c2a36e8efd8b8cc8884fa59
Michael Fleming <Michael@The-Original-Michaels-MacBook-Pro.local>
2013-12-05 21:24:21 -0500

    [delivers #61920348]
    Beginning to add login functionality and authorization with page access

--- js/controllers/LoginController.js
+++ js/controllers/LoginController.js
@@ -7,19 +7,22 @@ wingaming.controller('Login', ['$scope', '$routeParams', '$location', 'angularFi
 	$scope.users = angularFireCollection(urlUsers);
 	
     $scope.$on("angularFireAuth:login", function(evt, user) {
+    	console.log(user);
+    	console.log(user.email);
         if (user.provider == "facebook") {
             var picurl = "http://graph.facebook.com/" + user.username + "/picture?type=small";
             var picurlLarge = "http://graph.facebook.com/" + user.username + "/picture?type=large";
             $scope.users.add({"displayName": user.name, "email": user.email, "profilePic": picurl, "profilePicLarge": picurlLarge});
+            $location.path('/gts');
         } else {
 	        console.log("Login other then facebook");
         }
     });
 
     $scope.login = function() {
-        angularFireAuth.login('facebook').then(function() {
-            $location.path('/gts');
-        });
+		angularFireAuth.login("facebook", {
+			scope: "email"			
+		});
     };
     
     $scope.logout = function() {
--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var wingaming = angular.module('wingaming', ['firebase', 'crudControllers']);
 
 wingaming.run(['angularFireAuth', '$rootScope', function(angularFireAuth, $rootScope){
     var url = new Firebase("https://wingaminglounge.firebaseio.com/");
-    angularFireAuth.initialize(url, {scope: $rootScope, name: "user",path: '/home'});
+    angularFireAuth.initialize(url, {scope: $rootScope, name: "user",path: '/'});
 
     $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
         $rootScope.title = current.$$route.title;
@@ -11,27 +11,31 @@ wingaming.run(['angularFireAuth', '$rootScope', function(angularFireAuth, $rootS
 
 wingaming.config(function ($routeProvider){
     $routeProvider
-        .when("/",{
+        .when("/" , {
             title: 'Home',
+            controller: "Login",
             templateUrl:"views/home.html"
         })
         .when("/under_construction", {
             title: 'Work in progress',
+            authRequired: false,
             templateUrl:"views/under_construction.html"
         })
         .when("/gts", {
             title: 'Gamer Tracking System',
+            authRequired: true,
             templateUrl:"views/gts.html"
         })
         .when("/crud_testing", {
             title: 'Crud Testing',
+            authRequired: true,
             templateUrl:"views/crud_testing.html"
         })
         .otherwise({
         	redirectTo:"/",
         	title: "Home"
         });
-
+        
 }).directive('autoComplete', function($timeout) {
         return function(scope, iElement, iAttrs) {
             iElement.autocomplete({
