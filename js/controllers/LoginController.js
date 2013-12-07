3372276815417a341f5b76554aaf4f68005d1c9f
eangeles <niwre2007@gmail.com>
2013-12-07 17:22:57 -0500

    Commiting my demo code for Autocomeplete, which is also mobile friendly

--- js/controllers/LoginController.js
+++ js/controllers/LoginController.js
@@ -1,59 +1,66 @@
-wingaming.controller('Login', ['$scope', '$routeParams', '$location', 'angularFireCollection', 'angularFireAuth', function mtCtrl($scope, $routeParams, $location, angularFireCollection, angularFireAuth){
+wingaming.controller('Login', ['$scope', '$routeParams', '$location', 'angularFireCollection', 'angularFireAuth', function mtCtrl($scope, $routeParams, $location, angularFireCollection, angularFireAuth,$http){
 
-	var theUser;
+    var theUser;
 
     var urlUsers = new Firebase("https://wingaminglounge.firebaseio.com/wingaminglounge/users");
 
-	$scope.users = angularFireCollection(urlUsers);
-	
+    $scope.users = angularFireCollection(urlUsers);
+
     $scope.$on("angularFireAuth:login", function(evt, user) {
-        if (user.provider == "facebook") {            
+        if (user.provider == "facebook") {
             theUser = user;
         } else {
-	        console.log("Login other then the facebook service");
+            console.log("Login other then the facebook service");
         }
     });
 
+
+
     $scope.login = function() {
-		angularFireAuth.login("facebook", {
-			scope: "email"			
-		}).then(function(){	
-			var userExists;		
-            for (var i = 0, max = $scope.users.length; i<max; i++) {
-                if ($scope.users[i].email != theUser.email) {
-                	userExists = false;                
-                } else {
-                	userExists = true;
-                	theUser = $scope.users[i];
-                    break;
+        angularFireAuth.login("facebook", {
+            scope: "email"
+        }).then(function(){
+                var userExists;
+                for (var i = 0, max = $scope.users.length; i<max; i++) {
+                    if ($scope.users[i].email != theUser.email) {
+                        userExists = false;
+                    } else {
+                        userExists = true;
+                        theUser = $scope.users[i];
+                        break;
+                    }
                 }
-            }
-                        
-            if (!userExists) {
-	            console.log("User does not exist, adding " + theUser.email + " to the database.");
-	            
-	            var picurl = "http://graph.facebook.com/" + theUser.username + "/picture?type=small";
-		        var picurlLarge = "http://graph.facebook.com/" + theUser.username + "/picture?type=large";
-				
-				$scope.users.add({"displayName": theUser.name, "email": theUser.email, "profilePic": picurl, "profilePicLarge": picurlLarge});
-				$location.path('/game_page');
-            } else {
-	            console.log("User does exist, not adding " + theUser.email + " to the database");
-	            if (!theUser.userType) {
-		            $scope.user.userType="Gamer";
-		            $location.path("/game_page");
-	            } else {
-		            $scope.user.userType="Admin";
-		            $location.path("/gts");
-	            };
-            }
-			
-		});		
+
+                if (!userExists) {
+                    console.log("User does not exist, adding " + theUser.email + " to the database.");
+
+                    var picurl = "http://graph.facebook.com/" + theUser.username + "/picture?type=small";
+                    var picurlLarge = "http://graph.facebook.com/" + theUser.username + "/picture?type=large";
+
+                    $scope.users.add({"displayName": theUser.name, "email": theUser.email, "profilePic": picurl, "profilePicLarge": picurlLarge});
+                    $location.path('/game_page');
+                } else {
+                    console.log("User does exist, not adding " + theUser.email + " to the database");
+                    if (!theUser.userType) {
+                        $scope.user.userType="Gamer";
+                        $location.path("/game_page");
+                    } else {
+                        $scope.user.userType="Admin";
+                        $location.path("/gts");
+                    };
+                }
+
+            });
     };
-    
+
     $scope.logout = function() {
         angularFireAuth.logout();
         $location.path('/');
     };
 
+    //Filter gamer search and select to input
+    $scope.limit = 5;
+    $scope.setGamer = function (gamer) {
+        $scope.displayName = gamer; };
+    console.log();// pull selected city using {{selected | json}}
 }])
