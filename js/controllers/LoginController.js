ffafa7d4fcc81746bddbb08427e6c4e0949b3f63
Michael Fleming <Michael@The-Original-Michaels-MacBook-Pro.local>
2013-12-07 17:45:15 -0500

    Removing duplicated code

--- js/controllers/LoginController.js
+++ js/controllers/LoginController.js
@@ -15,42 +15,6 @@ wingaming.controller('Login', ['$scope', '$routeParams', '$location', 'angularFi
     });
 
     $scope.login = function() {
-
-        angularFireAuth.login("facebook", {
-            scope: "email"
-        }).then(function(){
-                var userExists;
-                for (var i = 0, max = $scope.users.length; i<max; i++) {
-                    if ($scope.users[i].email != theUser.email) {
-                        userExists = false;
-                    } else {
-                        userExists = true;
-                        theUser = $scope.users[i];
-                        break;
-                    }
-                }
-
-                if (!userExists) {
-                    console.log("User does not exist, adding " + theUser.email + " to the database.");
-
-                    var picurl = "http://graph.facebook.com/" + theUser.username + "/picture?type=small";
-                    var picurlLarge = "http://graph.facebook.com/" + theUser.username + "/picture?type=large";
-
-                    $scope.users.add({"displayName": theUser.name, "email": theUser.email, "profilePic": picurl, "profilePicLarge": picurlLarge, "userType": "Gamer"});
-                    $location.path('/game_page');
-                } else {
-                    console.log("User does exist, not adding " + theUser.email + " to the database");
-                    if (theUser.userType == "Gamer") {
-                        $scope.user.userType = false;
-                        $location.path("/game_page");
-                    } else if (theUser.userType == "Admin") {
-                        $scope.user.userType= true;
-                        $location.path("/gts");
-                    };
-                }
-
-            });
-
 		angularFireAuth.login("facebook", {
 			scope: "email"			
 		}).then(function(){	
@@ -75,7 +39,7 @@ wingaming.controller('Login', ['$scope', '$routeParams', '$location', 'angularFi
 				$location.path('/game_page');
             } else {
 	            console.log("User does exist, not adding " + theUser.email + " to the database");
-	            if (theUser.userType == "Gamer") {
+	            if (theUser.userType = "Gamer") {
 		            $scope.user.userType = false;
 		            $location.path("/game_page");
 	            } else if (theUser.userType == "Admin") {
@@ -85,27 +49,24 @@ wingaming.controller('Login', ['$scope', '$routeParams', '$location', 'angularFi
             }
 			
 		});		
-
     };
     
     $scope.logout = function() {
         angularFireAuth.logout();
         $location.path('/');
     };
-
+    
     var false_statement = "<h1>Your not an Admin</h1>";
     var true_statement = "<aside class=\"right-off-canvas-menu\"><ul class=\"off-canvas-list\"><li><label><a href=\"#/gts\">Admin</a> </label></li></ul></aside>";
-
-    $scope.render = function(condition) {
-        return condition ? true_statement : false_statement;
-    };
-
-
-    //Filter gamer search and select to input
+    
+    $scope.render = function(condition) {        
+	    return condition ? true_statement : false_statement;
+	};
+	
+	//Filter gamer search and select to input
     $scope.limit = 5;
     $scope.setGamer = function (gamer) {
         $scope.displayName = gamer; };
     // pull selected city using {{selected | json}}
-
 
 }])
